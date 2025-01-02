6c679faaa4759beef85ff653d01fc49450b3f9c0
Ankit Partap <ankitpartap24@gmail.com>
2025-01-02 23:54:52 +0530

    Cards, done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
+import Cards from '../components/Cards'
+import { Modal } from 'antd';
+
 
 function Dashboard() {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  }
+
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  }
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  }
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  }
+
   return (
     <>
-    <Header/>
-    <div>Dashboard Page</div>
+      <div>
+        <Header />
+        <Cards
+          showExpenseModal={showExpenseModal}
+          showIncomeModal={showIncomeModal}
+        />
+        <Modal visible={isIncomeModalVisible} onCancel={handleIncomeCancel} title="Add Income" footer={null}>Income</Modal>
+        <Modal visible={isExpenseModalVisible} onCancel={handleExpenseCancel} title="Add Expense" footer={null}>Expense</Modal>
+      </div>
     </>
   )
 }
