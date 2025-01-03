967e1d59a6bd072234539f1fa217c5e782acb708
Ankit Partap <ankitpartap24@gmail.com>
2025-01-03 11:59:12 +0530

    Modals added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
 import Cards from '../components/Cards'
-import { Modal } from 'antd';
+// import { Modal } from 'antd';
+import AddExpenseModal from '../components/Modals/addExpense';
+import AddIncomeModal from '../components/Modals/addIncome';
 
 
 function Dashboard() {
@@ -20,9 +22,14 @@ function Dashboard() {
     setIsExpenseModalVisible(false);
   }
   const handleIncomeCancel = () => {
+    console.log("income cancle");
     setIsIncomeModalVisible(false);
   }
 
+  const onFinish=(values, type)=>{
+    console.log("Finish: ", values, type);
+    
+  }
   return (
     <>
       <div>
@@ -31,8 +38,10 @@ function Dashboard() {
           showExpenseModal={showExpenseModal}
           showIncomeModal={showIncomeModal}
         />
-        <Modal visible={isIncomeModalVisible} onCancel={handleIncomeCancel} title="Add Income" footer={null}>Income</Modal>
-        <Modal visible={isExpenseModalVisible} onCancel={handleExpenseCancel} title="Add Expense" footer={null}>Expense</Modal>
+        {/* <Modal visible={isIncomeModalVisible} onCancel={handleIncomeCancel} title="Add Income" footer={null}>Income</Modal> */}
+        <AddExpenseModal isExpenseModalVisible={isExpenseModalVisible} handleExpenseCancel={handleExpenseCancel} onFinish={onFinish}/>
+        <AddIncomeModal isIncomeModalVisible={isIncomeModalVisible} handleIncomeCancel={handleIncomeCancel} onFinish={onFinish}/>
+        {/* <Modal visible={isExpenseModalVisible} onCancel={handleExpenseCancel} title="Add Expense" footer={null}>Expense</Modal> */}
       </div>
     </>
   )
